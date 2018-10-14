a59ef21f06c51fcc3364e2da56691d8e26cd819f
squaredcanine <quintenstekie@gmail.com>
2018-10-14 13:35:59 +0200

    Bug fix and implementation of opening and closing

--- Form1.cs
+++ Form1.cs
@@ -105,19 +105,26 @@ namespace INFOIBV
             progressBar.Step = 1;
             //Reads the combobox to decide which conversion should be done on the input image.
             Tuple<int, int>[] tupleList;
+            bool checkeredcheckbox = checkBox1.Checked;
             switch (comboBox1.Text)
             {
                 case "erosion":
                     tupleList = convertInputToTuples();
-                    Image = conversionErosion(Image, tupleList, isBinaryButton.Checked);
+                    Image = conversionErosion(Image, tupleList, checkeredcheckbox);
                     break;
                 case "dilation":
                     tupleList = convertInputToTuples();
-                    Image = conversionDilation(Image, tupleList, isBinaryButton.Checked);
+                    Image = conversionDilation(Image, tupleList, checkeredcheckbox);
                     break;
                 case "opening":
+                    tupleList = convertInputToTuples();
+                    Image = conversionErosion(Image, tupleList, checkeredcheckbox);
+                    Image = conversionDilation(Image, tupleList, checkeredcheckbox);
                     break;
                 case "closing":
+                    tupleList = convertInputToTuples();
+                    Image = conversionDilation(Image, tupleList, checkeredcheckbox);
+                    Image = conversionErosion(Image, tupleList, checkeredcheckbox);
                     break;
                 case "complement":
                     Image = conversionComplement(Image);
@@ -266,8 +273,6 @@ namespace INFOIBV
                     }
                 }
             }
-            Console.WriteLine("Breakpoint");
-
             return newImage;
         }
 
@@ -312,7 +317,11 @@ namespace INFOIBV
         private Color[,] conversionErosionGrayscale(Color[,] image, Tuple<int, int>[] kernel)
         {
             Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
-                                List<int> valueList = new List<int>();
+            for (int x = 0; x < InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < InputImage.Size.Height; y++)
+                {
+                    List<int> valueList = new List<int>();
                     for (int structureIndex = 0; structureIndex < kernel.Length; structureIndex++)
                     {
                         int structureX = x + kernel[structureIndex].Item1;
@@ -321,7 +330,7 @@ namespace INFOIBV
                         if (!(structureX < 0 || structureY < 0 || structureY > (InputImage.Size.Height - 1) ||
                             structureX > (InputImage.Size.Width - 1 )))
                         {
-                            valueList.Add(image[structureX, structureY].R);
+                                valueList.Add(image[structureX, structureY].R);
                         }
                     }
 
@@ -329,8 +338,6 @@ namespace INFOIBV
                     newImage[x, y] = Color.FromArgb(newColor, newColor, newColor);
                 }
             }
-            Console.WriteLine("Breakpoint");
-
             return newImage;
         }
 
@@ -414,7 +421,7 @@ namespace INFOIBV
                 return image;
             }
             return null;
-
+        }
         //Counts the amount of distinct values in a given image ;we assume that the image is a greyscale
         //Will count the amount of distinct green values when applied to a colored image
         private int countDistinctValues(Color[,] image)
@@ -969,12 +976,12 @@ namespace INFOIBV
             if (comboBox1.Text.Equals("erosion") || comboBox1.Text.Equals("dilation") ||
                 comboBox1.Text.Equals("opening") || comboBox1.Text.Equals("closing"))
             {
-                isBinaryButton.Visible = true;
+                checkBox1.Visible = true;
             }
 
             else
             {
-                isBinaryButton.Visible = false;
+                checkBox1.Visible = false;
             }
 
         }
